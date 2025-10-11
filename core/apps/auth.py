5f9e9b5763f683dc7f031673b448be88a3682ef7
jero98772 <jero98772@protonmail.com>
2025-10-11 15:07:05 -0500

    update

---
+++ core/apps/auth.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-"
+#Epam_assesment - by Jero98772
---
+++ core/db/models.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-"
+#Epam_assesment - by Jero98772
+"""
+File database information
+"""
